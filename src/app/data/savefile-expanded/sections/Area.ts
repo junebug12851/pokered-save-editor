cb4ed4d2cda2c33d84ddd4ff9476b357bda78499
June Hanabi <junehanabi@gmail.com>
2018-08-16 19:16:23 -0400

    Various fixes and missing additions

--- src/app/data/savefile-expanded/sections/Area.ts
+++ src/app/data/savefile-expanded/sections/Area.ts
@@ -153,16 +153,88 @@ export class Area {
         this.tilesetType = saveFile.getByte(0x3522);
     }
 
+    // General
+    public contrast: number;
+    public noLetterDelay: boolean;
+    public countPlaytime: boolean;
+
+    // Audio
     public musicID: number;
     public musicBank: number;
-    public contrast: number;
-    public curMap: number;
-    public currentTileBlockMapViewPointer: number;
+    public noAudioFadeout: boolean;
+    public preventMusicChange: boolean;
+
+    // Tileset
+    public curTileset: number;
+    public tilesetBank: number;
+    public outOfBoundsTile: number;
+    public tilesetBlockPtr: number;
+    public tilesetGfxPtr: number;
+    public tilesetCollPtr: number;
+    public tilesetTalkingOverTiles: Uint8Array;
+    public tilesetGrassTiles: Uint8Array;
+    public boulderSpriteIndex: number;
+    public tileFrontBoulderColl: number;
+    public tilesetType: number;
+
+    // Sprites
+    public spriteSet: Uint8Array;
+    public spriteSetId: number;
+    public spriteData: SpriteData[];
+    public tradeCenterSpritesFaced: boolean;
+    public extendedSpriteData: SpriteDataExtended[];
+
+    // Warps
+    public warpData: WarpData[];
+    public warpDest: number;
+    public dungeonWarpDest: number;
+    public destinationMap: number;
+    public whichDungeonWarp: number;
+    public warpToLavenderTown: boolean;
+    public isDungeonWarp: boolean;
+    public flyOrDungeonWarp: boolean;
+    public flyWarp: boolean;
+    public dungeonWarp: boolean;
+    public skipJoypadCheckWarps: boolean;
+    public warpedFromWarp: number;
+    public warpedfromMap: number;
+
+    // Signs
+    public signData: SignData[];
+
+    // Player
+    public playerMoveDir: number;
+    public playerLastStopDir: number;
+    public playerCurDir: number;
     public yCoord: number;
     public xCoord: number;
     public yBlockCoord: number;
     public xBlockCoord: number;
-    public curTileset: number;
+    public yOffsetSinceLastSpecialWarp: number;
+    public xOffsetSinceLastSpecialWarp: number;
+    public walkBikeSurf: number;
+    public safariSteps: number;
+    public playerJumpingYScrnCoords: number;
+    public strengthOutsideBattle: boolean;
+    public surfingAllowed: boolean;
+    public usedCardKey: boolean;
+    public isBattle: boolean;
+    public isTrainerBattle: boolean;
+    public noBattles: boolean;
+    public battleEndedOrBlackout: boolean;
+    public usingLinkCable: boolean;
+    public flyOutofBattle: boolean;
+    public standingOnDoor: boolean;
+    public movingThroughDoor: boolean;
+    public standingOnWarp: boolean;
+    public finalLedgeJumping: boolean;
+    public spinPlayer: boolean;
+    public safariGameOver: number;
+    public safariBallCount: number;
+
+    // Map
+    public curMap: number;
+    public currentTileBlockMapViewPointer: number;
     public mapHeight: number;
     public mapWidth: number;
     public mapDataPtr: number;
@@ -180,89 +252,36 @@ export class Area {
         west: MapConnData,
         east: MapConnData,
     };
-    public spriteSet: Uint8Array;
-    public spriteSetId: number;
-    public outOfBoundsTile: number;
-    public warpData: WarpData[];
-    public warpDest: number;
-    public signData: SignData[];
-    public spriteData: SpriteData[];
-    public yOffsetSinceLastSpecialWarp: number;
-    public xOffsetSinceLastSpecialWarp: number;
     public map2x2Height: number;
     public map2x2Width: number;
     public mapViewVRAMPointer: number;
-    public playerMoveDir: number;
-    public playerLastStopDir: number;
-    public playerCurDir: number;
-    public tilesetBank: number;
-    public tilesetBlockPtr: number;
-    public tilesetGfxPtr: number;
-    public tilesetCollPtr: number;
-    public tilesetTalkingOverTiles: Uint8Array;
-    public tilesetGrassTiles: Uint8Array;
-
-    public missableList: Missable[];
-    public walkBikeSurf: number;
-    public safariSteps: number;
-    public playerJumpingYScrnCoords: number;
-    public boulderSpriteIndex: number;
-    public tileFrontBoulderColl: number;
-    public dungeonWarpDest: number;
-    public destinationMap: number;
-    public whichDungeonWarp: number;
-
-    public strengthOutsideBattle: boolean;
-    public surfingAllowed: boolean;
-    public usedCardKey: boolean;
     public pauseWildEncounters3Steps: boolean;
-    public noAudioFadeout: boolean;
-    public tradeCenterSpritesFaced: boolean;
-    public warpToLavenderTown: boolean;
-    public isDungeonWarp: boolean;
-    public npcsFaceAway: boolean;
-    public isBattle: boolean;
-    public isTrainerBattle: boolean;
-    public noBattles: boolean;
-    public battleEndedOrBlackout: boolean;
-    public usingLinkCable: boolean;
-    public scriptedNPCMovement: boolean;
-    public npcSpriteMovement: boolean;
-    public ignoreJoypad: boolean;
-    public noLetterDelay: boolean;
-    public joypadSimulation: boolean;
-    public countPlaytime: boolean;
-    public flyOrDungeonWarp: boolean;
-    public flyWarp: boolean;
-    public dungeonWarp: boolean;
     public forceBikeRide: boolean;
     public blackoutDest: boolean;
-    public runningTestBattle: boolean;
-    public preventMusicChange: boolean;
-    public skipJoypadCheckWarps: boolean;
-    public trainerWantsBattle: boolean;
     public curMapNextFrame: boolean;
-    public flyOutofBattle: boolean;
-    public standingOnDoor: boolean;
-    public movingThroughDoor: boolean;
-    public standingOnWarp: boolean;
-    public finalLedgeJumping: boolean;
-    public spinPlayer: boolean;
-    public warpedFromWarp: number;
-    public warpedfromMap: number;
     public cardKeyDoorY: number;
     public cardKeyDoorX: number;
-    public firstTrashcanLock: number;
-    public secondTrashcanLock: number;
     public grassRate: number;
     public grassPokemon: Uint8Array;
     public waterPokemonRate: number;
     public waterPokemon: Uint8Array; // 20 entries
-    public trainerHeaderPtr: number;
-    public oppAfterWrongAnsw: number;
     public curMapScript: number;
-    public safariGameOver: number;
-    public safariBallCount: number;
-    public extendedSpriteData: SpriteDataExtended[];
-    public tilesetType: number;
+
+    // NPC
+    public npcsFaceAway: boolean;
+    public scriptedNPCMovement: boolean;
+    public npcSpriteMovement: boolean;
+    public ignoreJoypad: boolean;
+    public joypadSimulation: boolean;
+    public runningTestBattle: boolean;
+    public trainerWantsBattle: boolean;
+    public trainerHeaderPtr: number;
+
+    // Missable
+    public missableList: Missable[];
+
+    // Puzzle
+    public firstTrashcanLock: number;
+    public secondTrashcanLock: number;
+    public oppAfterWrongAnsw: number;
 }
