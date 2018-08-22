9697d38d63fa63683d8fe8a6d7ad3a9970e4426a
June Hanabi <junehanabi@gmail.com>
2018-08-22 11:29:52 -0400

    Completed Player/Pokedex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 // Modules
 import { FontawesomeModule } from './libs/fontawesome.module';
@@ -81,7 +82,7 @@ import { AreaSpritesComponent } from './screens/area-sprites/area-sprites.compon
 import { AreaWarpsComponent } from './screens/area-warps/area-warps.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatToolbarModule, MatIconModule } from '@angular/material';
 
 @NgModule({
     declarations: [
