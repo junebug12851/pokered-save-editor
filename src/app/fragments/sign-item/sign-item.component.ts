43196948bebbd5895a894ec26833a44559d367d5
June Hanabi <junehanabi@gmail.com>
2018-08-24 06:31:27 -0400

    Added and Completed  Area/Signs

---
+++ src/app/fragments/sign-item/sign-item.component.ts
@@ -0,0 +1,54 @@
+import { ValueAccessorBase } from './../abstract/ValueAccessorBase';
+/**
+   Copyright 2018 June Hanabi
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+import { Component, EventEmitter, Output, Input, OnInit } from '@angular/core';
+
+import {
+    NG_VALUE_ACCESSOR,
+} from '@angular/forms';
+
+@Component({
+    selector: 'sign-item',
+    templateUrl: './sign-item.component.pug',
+    styleUrls: ['./sign-item.component.scss'],
+    providers: [
+        { provide: NG_VALUE_ACCESSOR, useExisting: SignItemComponent, multi: true }
+    ],
+})
+export class SignItemComponent extends ValueAccessorBase<string> implements OnInit {
+
+    constructor() {
+        super();
+    }
+
+    ngOnInit() {
+
+    }
+
+    @Input()
+    public disabled: boolean = false;
+
+    @Input()
+    public data: any = { x: 0, y: 0, text: 0 };
+
+    @Output()
+    public remove: EventEmitter<boolean> = new EventEmitter();
+
+    remListItem() {
+        this.remove.emit(true);
+    }
+}
