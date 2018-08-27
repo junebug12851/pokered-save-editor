86236ea6c3d9e531446c80c6d16f6b49e974d235
June Hanabi <junehanabi@gmail.com>
2018-08-27 10:25:59 -0400

    Completed World/Trades

---
+++ src/app/data/trades.ts
@@ -0,0 +1,12 @@
+export const trades = [
+    { give: "NIDORINO", get: "NIDORINA", textId: 0x0, nickname: "TERRY" },
+    { give: "ABRA", get: "MR.MIME", textId: 0x0, nickname: "MARCEL" },
+    { give: "BUTTERFREE", get: "BEEDRILL", textId: 0x2, nickname: "CHIKUCHIKU" },
+    { give: "PONYTA", get: "SEEL", textId: 0x0, nickname: "SAILOR" },
+    { give: "SPEAROW", get: "FARFETCHD", textId: 0x2, nickname: "DUX" },
+    { give: "SLOWBRO", get: "LICKITUNG", textId: 0x0, nickname: "MARC" },
+    { give: "POLIWHIRL", get: "JYNX", textId: 0x1, nickname: "LOLA" },
+    { give: "RAICHU", get: "ELECTRODE", textId: 0x1, nickname: "DORIS" },
+    { give: "VENONAT", get: "TANGELA", textId: 0x2, nickname: "CRINKLES" },
+    { give: "NIDORAN<GM>", get: "NIDORAN<GF>", textId: 0x2, nickname: "SPOT" },
+];
