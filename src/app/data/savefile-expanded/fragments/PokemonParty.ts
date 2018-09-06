def52d05334989be39dea8fc697686de14c912e5
June Hanabi <junehanabi@gmail.com>
2018-09-06 12:14:38 -0400

    More progress in refactoring writeBack into SaveFileExpanded and cleaning up SaveFileExpanded

--- src/app/data/savefile-expanded/fragments/PokemonParty.ts
+++ src/app/data/savefile-expanded/fragments/PokemonParty.ts
@@ -1,59 +1,66 @@
 import { SaveFileIterator } from './../SaveFileIterator';
 import { SaveFileService } from './../../savefile.service';
-import { PokemonBox, PokemonBoxData } from './PokemonBox';
+import { PokemonBox } from './PokemonBox';
 
-export interface PokemonPartyData extends PokemonBoxData {
-    maxHP: number;
-    attack: number;
-    defense: number;
-    speed: number;
-    special: number;
-}
-
-export class PokemonParty extends PokemonBox implements PokemonPartyData {
-    constructor(saveFile: SaveFileService,
-        offset: number,
-        nicknameStartOffset: number,
-        otNameStartOffset: number,
-        index: number) {
+export class PokemonParty extends PokemonBox {
+    constructor(saveFile?: SaveFileService,
+        offset?: number,
+        nicknameStartOffset?: number,
+        otNameStartOffset?: number,
+        index?: number) {
 
         // Mark record size at end to the expanded size of 0x2C
         // Pokemon Party has expanded data
         super(saveFile, offset, nicknameStartOffset, otNameStartOffset, index, 0x2C);
+    }
 
-        const it: SaveFileIterator = this.it;
+    public load(saveFile: SaveFileService,
+        offset: number,
+        nicknameStartOffset: number,
+        otNameStartOffset: number,
+        index: number): SaveFileIterator {
+
+        const it: SaveFileIterator = super.load(saveFile,
+            offset,
+            nicknameStartOffset,
+            otNameStartOffset,
+            index);
+
         this.level = it.getByte();
         this.maxHP = it.getWord();
         this.attack = it.getWord();
         this.defense = it.getWord();
         this.speed = it.getWord();
         this.special = it.getWord();
+
+        return it;
     }
 
-    static get empty(): PokemonPartyData {
-        const parent: any = PokemonBox.empty;
-        parent.maxHP = 0;
-        parent.attack = 0;
-        parent.defense = 0;
-        parent.speed = 0;
-        parent.special = 0;
-        return parent;
-    }
+    public save(saveFile: SaveFileService,
+        offset: number,
+        nicknameStartOffset: number,
+        otNameStartOffset: number,
+        index: number): SaveFileIterator {
 
-    public save(): SaveFileIterator {
-        const it = super.save();
+        const it: SaveFileIterator = super.save(saveFile,
+            offset,
+            nicknameStartOffset,
+            otNameStartOffset,
+            index);
+
         it.setByte(this.level);
         it.setWord(this.maxHP);
         it.setWord(this.attack);
         it.setWord(this.defense);
         it.setWord(this.speed);
         it.setWord(this.special);
+
         return it;
     }
 
-    public maxHP: number;
-    public attack: number;
-    public defense: number;
-    public speed: number;
-    public special: number;
+    public maxHP: number = 0;
+    public attack: number = 0;
+    public defense: number = 0;
+    public speed: number = 0;
+    public special: number = 0;
 }
--- src/app/data/savefile-expanded/fragments/SignData.ts
+++ src/app/data/savefile-expanded/fragments/SignData.ts
@@ -1,16 +1,12 @@
 import { SaveFileService } from './../../savefile.service';
 
-export interface SignDataData {
-    y: number;
-    x: number;
-    text: number;
-}
+export class SignData {
+    constructor(savefile?: SaveFileService, index?: number) {
+        if (arguments.length >= 2)
+            this.load(savefile as SaveFileService, index as number);
+    }
 
-export class SignData implements SignDataData {
-    constructor(savefile: SaveFileService, index: number) {
-        this.saveFile = savefile;
-        this.index = index;
-
+    public load(savefile: SaveFileService, index: number) {
         const it = savefile.iterator.offsetTo((2 * index) + 0x275D);
         this.y = it.getByte();
         this.x = it.getByte();
@@ -19,18 +15,7 @@ export class SignData implements SignDataData {
         this.text = it.getByte();
     }
 
-    static get empty(): SignDataData {
-        return {
-            y: 0,
-            x: 0,
-            text: 0
-        }
-    }
-
-    public save() {
-        const saveFile = this.saveFile;
-        const index = this.index;
-
+    public save(saveFile: SaveFileService, index: number) {
         const it = saveFile.iterator.offsetTo((2 * index) + 0x275D);
         it.setByte(this.y);
         it.setByte(this.x);
@@ -39,10 +24,7 @@ export class SignData implements SignDataData {
         it.setByte(this.text);
     }
 
-    public saveFile: SaveFileService;
-    public index: number;
-
-    public y: number;
-    public x: number;
-    public text: number;
+    public y: number = 0;
+    public x: number = 0;
+    public text: number = 0;
 }
