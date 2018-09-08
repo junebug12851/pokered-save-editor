efabe708d1bf34b9040720123c55359e51486388
June Hanabi <junehanabi@gmail.com>
2018-09-08 05:02:17 -0400

    Completed cleaning up Storage

--- src/app/data/savefile-expanded/sections/Storage.ts
+++ src/app/data/savefile-expanded/sections/Storage.ts
@@ -2,14 +2,18 @@ import { PokemonBox } from './../fragments/PokemonBox';
 import { SaveFileService } from '../../savefile.service';
 
 export class Storage {
-    constructor(saveFile: SaveFileService) {
+    constructor(saveFile?: SaveFileService) {
+        if (arguments.length >= 1)
+            this.load(saveFile as SaveFileService);
+    }
+
+    load(saveFile: SaveFileService) {
         let curBox = this.curBox = (saveFile.getByte(0x284C) & 0b01111111);
-        curBox = this.curBox += 1;
 
         // All the Pokemon data is uninitialized and unformatted / random
         // garbage if this is false
         // So if this is false then don't load any box data
-        const noFormat = this.changedBoxesBefore = saveFile.getBit(0x284C, 0x1, 7);
+        this.changedBoxesBefore = saveFile.getBit(0x284C, 0x1, 7);
 
         const it = saveFile.iterator.offsetTo(0x27E7);
         this.boxItems = [];
@@ -23,151 +27,11 @@ export class Storage {
         // Create Parent of all boxes
         this.pokemonBoxes = [];
 
-        // Box 1
-        this.pokemonBoxes.push([]);
-        if (noFormat)
-            for (let i = 0; i < saveFile.getByte(0x4000) && i < 20; i++) {
-                this.pokemonBoxes[0].push(new PokemonBox(
-                    saveFile,
-                    0x4016,
-                    0x4386,
-                    0x42AA,
-                    i));
-            }
+        // Load boxes 1-6
+        this.loadBoxes(saveFile, 0x4000);
 
-        // Box 2
-        this.pokemonBoxes.push([]);
-        if (noFormat)
-            for (let i = 0; i < saveFile.getByte(0x4462) && i < 20; i++) {
-                this.pokemonBoxes[1].push(new PokemonBox(
-                    saveFile,
-                    0x4478,
-                    0x47E8,
-                    0x470C,
-                    i));
-            }
-
-        // Box 3
-        this.pokemonBoxes.push([]);
-        if (noFormat)
-            for (let i = 0; i < saveFile.getByte(0x48C4) && i < 20; i++) {
-                this.pokemonBoxes[2].push(new PokemonBox(
-                    saveFile,
-                    0x48DA,
-                    0x4C4A,
-                    0x4B6E,
-                    i));
-            }
-
-        // Box 4
-        this.pokemonBoxes.push([]);
-        if (noFormat)
-            for (let i = 0; i < saveFile.getByte(0x4D26) && i < 20; i++) {
-                this.pokemonBoxes[3].push(new PokemonBox(
-                    saveFile,
-                    0x4D3C,
-                    0x50AC,
-                    0x4FD0,
-                    i));
-            }
-
-        // Box 5
-        this.pokemonBoxes.push([]);
-        if (noFormat)
-            for (let i = 0; i < saveFile.getByte(0x5188) && i < 20; i++) {
-                this.pokemonBoxes[4].push(new PokemonBox(
-                    saveFile,
-                    0x519E,
-                    0x550E,
-                    0x5432,
-                    i));
-            }
-
-        // Box 6
-        this.pokemonBoxes.push([]);
-        if (noFormat)
-            for (let i = 0; i < saveFile.getByte(0x55EA) && i < 20; i++) {
-                this.pokemonBoxes[5].push(new PokemonBox(
-                    saveFile,
-                    0x5600,
-                    0x5970,
-                    0x5894,
-                    i));
-            }
-
-        ///////////////////////////////////////////////////////
-
-        // Box 7
-        this.pokemonBoxes.push([]);
-        if (noFormat)
-            for (let i = 0; i < saveFile.getByte(0x6000) && i < 20; i++) {
-                this.pokemonBoxes[6].push(new PokemonBox(
-                    saveFile,
-                    0x6016,
-                    0x6386,
-                    0x62AA,
-                    i));
-            }
-
-        // Box 8
-        this.pokemonBoxes.push([]);
-        if (noFormat)
-            for (let i = 0; i < saveFile.getByte(0x6462) && i < 20; i++) {
-                this.pokemonBoxes[7].push(new PokemonBox(
-                    saveFile,
-                    0x6478,
-                    0x67E8,
-                    0x670C,
-                    i));
-            }
-
-        // Box 9
-        this.pokemonBoxes.push([]);
-        if (noFormat)
-            for (let i = 0; i < saveFile.getByte(0x68C4) && i < 20; i++) {
-                this.pokemonBoxes[8].push(new PokemonBox(
-                    saveFile,
-                    0x68DA,
-                    0x6C4A,
-                    0x6B6E,
-                    i));
-            }
-
-        // Box 10
-        this.pokemonBoxes.push([]);
-        if (noFormat)
-            for (let i = 0; i < saveFile.getByte(0x6D26) && i < 20; i++) {
-                this.pokemonBoxes[9].push(new PokemonBox(
-                    saveFile,
-                    0x6D3C,
-                    0x70AC,
-                    0x6FD0,
-                    i));
-            }
-
-        // Box 11
-        this.pokemonBoxes.push([]);
-        if (noFormat)
-            for (let i = 0; i < saveFile.getByte(0x7188) && i < 20; i++) {
-                this.pokemonBoxes[10].push(new PokemonBox(
-                    saveFile,
-                    0x719E,
-                    0x750E,
-                    0x7432,
-                    i));
-            }
-
-        // Box 12
-        this.pokemonBoxes.push([]);
-        if (noFormat)
-            for (let i = 0; i < saveFile.getByte(0x75EA) && i < 20; i++) {
-                this.pokemonBoxes[11].push(new PokemonBox(
-                    saveFile,
-                    0x7600,
-                    0x7970,
-                    0x7894,
-                    i));
-            }
+        // Load boxes 7-12
+        this.loadBoxes(saveFile, 0x6000);
 
         // Overwrite box data of the current box with current box data
         // Pokemon Red and Blue save a copy of the current box locally in Bank 1
@@ -179,23 +43,104 @@ export class Storage {
 
         // Wipe current box data and re-insert from scratch latest box data for
         // that box
-        this.pokemonBoxes[curBox - 1] = [];
-        if (noFormat)
-            for (let i = 0; i < saveFile.getByte(0x30C0) && i < 20; i++) {
-                this.pokemonBoxes[curBox - 1].push(new PokemonBox(
-                    saveFile,
-                    0x30D6,
-                    0x3446,
-                    0x336A,
-                    i));
-            }
+
+        // Also force load the current box
+        this.pokemonBoxes[curBox] = [];
+        this.loadBox(saveFile, curBox, 0x30C0, true);
     }
 
-    public curBox: number;
-    public changedBoxesBefore: boolean;
+    // Loads 6 boxes in a bank offset adding to the existing boxes
+    loadBoxes(saveFile: SaveFileService, boxesOffset: number) {
+        for (let i = 0; i < 6; i++) {
+            this.pokemonBoxes.push([]);
+            this.loadBox(
+                saveFile,
+                this.pokemonBoxes.length - 1,
+                (i * 0x462) + boxesOffset
+            )
+        }
+    }
+
+    // Loads a pokemon box # from given offset, won't load anything unless
+    // data is pre-formatted or forced
+    loadBox(saveFile: SaveFileService, boxInd: number, boxOffset: number, forceLoad: boolean = false) {
+        if (!this.changedBoxesBefore && !forceLoad)
+            return;
+
+        for (let i = 0; i < saveFile.getByte(boxOffset) && i < 20; i++) {
+            this.pokemonBoxes[boxInd].push(new PokemonBox(
+                saveFile,
+                boxOffset + 0x16,
+                boxOffset + 0x386,
+                boxOffset + 0x2AA,
+                i));
+        }
+    }
+
+    save(saveFile: SaveFileService) {
+        // Save current box details
+        saveFile.setByte(0x284C, this.curBox);
+        saveFile.setBit(0x284C, 1, 7, this.changedBoxesBefore);
+
+        // Save all box items
+        const it = saveFile.iterator.offsetTo(0x27E6);
+        it.setByte(this.boxItems.length);
+        for (let i = 0; i < this.boxItems.length && i < 50; i++) {
+            it.setByte(this.boxItems[i].id);
+            it.setByte(this.boxItems[i].amount);
+        }
+        it.setByte(0xFF);
+
+        // Save boxes 1-6
+        this.saveBoxes(
+            saveFile,
+            0x4000,
+            0
+        );
+
+        // Save boxes 7-12
+        this.saveBoxes(
+            saveFile,
+            0x6000,
+            1
+        );
+
+        // Save current box data to current box cache
+        this.saveBox(
+            saveFile,
+            this.curBox,
+            0x30C0
+        );
+    }
+
+    saveBox(saveFile: SaveFileService, boxInd: number, boxOffset: number) {
+        saveFile.setByte(boxOffset, this.pokemonBoxes[boxInd].length);
+        for (let i = 0; i < this.pokemonBoxes[boxInd].length && i < 20; i++) {
+            this.pokemonBoxes[boxInd][i].save(
+                saveFile,
+                boxOffset + 0x16,
+                boxOffset + 0x386,
+                boxOffset + 0x2AA,
+                i
+            );
+        }
+    }
+
+    saveBoxes(saveFile: SaveFileService, boxesOffset: number, boxSet: number) {
+        for (let i = boxSet * 6; i < (boxSet * 6) + 6; i++) {
+            this.saveBox(
+                saveFile,
+                i,
+                (i * 0x462) + boxesOffset
+            )
+        }
+    }
+
+    public curBox: number = 0;
+    public changedBoxesBefore: boolean = false;
     public boxItems: {
         id: number,
         amount: number
-    }[];
-    public pokemonBoxes: PokemonBox[][];
+    }[] = [];
+    public pokemonBoxes: PokemonBox[][] = [[]];
 }
