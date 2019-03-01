5d16bb2fa321b4291cd8c0f66680386ba1a90755
June Hanabi <junehanabi@gmail.com>
2019-03-01 10:31:13 -0500

    Added base stats and base exp yield to all Pokemon

--- src/assets/data/pokemon.d.ts
+++ src/assets/data/pokemon.d.ts
@@ -1,7 +1,23 @@
 export interface Pokemon {
-    name: string, // Pokemon Name (Nidoran will use codes ex: Nidroan<m>)
+
+    // For all Pokemon
+    name: string, // Pokemon Name with code (ex Nidoran will use ex: Nidroan<m>)
     ind: number, // Pokemon Index Order
+
+    // For valid Pokemon
+    pokedex?: number, // Pokedex Order
+    growthRate?: number, // Expirience Growth Rate Category
+
+    // Base Stats
+    baseHp?: number,
+    baseAttack?: number,
+    baseDefense?: number,
+    baseSpeed?: number,
+    baseSpecial?: number,
+
+    // Base EXP
+    baseExpYield?: number,
+
+    // For Invalid Pokemon
     glitch?: boolean, // Is this a glitch Pokemon?
-    pokedex?: number, // Pokedex Order if not glitch
-    growthRate?: number // Expirience Growth Rate Category
 };
