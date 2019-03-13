b26ad09341a4fb8ee37dc72508a1f7243dcec5b2
June Hanabi <junehanabi@gmail.com>
2019-03-13 00:47:07 -0400

    Added much more information to Pokemon.json

--- src/assets/data/pokemon.d.ts
+++ src/assets/data/pokemon.d.ts
@@ -20,6 +20,43 @@ export interface Pokemon {
     // Base EXP
     baseExpYield?: number,
 
+    // Naturally Learned Moves
+    moves?: {
+        level: number, // Level learned at
+        move: string // Move name
+    }[],
+
+    // Initial Moves immidiately available
+    initial?: string[];
+
+    // TM's and HM's Learnable
+    // Internally HM's are actually just part of the TM list
+    tmHm?: number[];
+
+    // Pokemon Types
+    type1?: string;
+    type2?: string;
+
+    // How easily catchable in wild
+    // Lower is more diffcult, higher is more easy
+    // Values 0-255
+    catchRate?: number;
+
+    // This Pokemon can evolve and this is how
+    evolution?: {
+        // The Pokemon evolves by reaching this level
+        level?: number;
+
+        // The Pokemon evolves by using this item
+        item?: string;
+
+        // The Pokemon evolves by trading it
+        trade?: true;
+
+        // The Pokemon evolves into this Pokemon
+        toName: string;
+    }
+
     // For Invalid Pokemon
     glitch?: boolean, // Is this a glitch Pokemon?
 };
